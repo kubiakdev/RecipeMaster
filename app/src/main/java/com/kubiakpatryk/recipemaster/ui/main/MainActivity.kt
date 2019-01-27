80f3c3abc4ca4a0aa6872741183df9387d3fd8d8
Patryk Kubiak <patryk.kubiak@appchance.com>
2019-01-27 13:40:34 +0100

    Changed main activity, added single line at the end of each class, connected to the API.

--- app/src/main/java/com/kubiakpatryk/recipemaster/ui/main/MainActivity.kt
+++ app/src/main/java/com/kubiakpatryk/recipemaster/ui/main/MainActivity.kt
@@ -1,10 +1,64 @@
 package com.kubiakpatryk.recipemaster.ui.main
 
+import android.os.Build
+import android.os.Bundle
+import android.widget.Toast
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.google.android.material.snackbar.Snackbar
 import com.kubiakpatryk.recipemaster.R
 import com.kubiakpatryk.recipemaster.ui.base.BaseActivity
+import com.kubiakpatryk.recipemaster.util.gone
+import com.kubiakpatryk.recipemaster.util.visible
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity<MainView, MainPresenter>(), MainView {
 
     override val layoutResId: Int = R.layout.activity_main
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        fabMainRecipe.setOnClickListener {
+            //todo implement switch to next activity
+        }
+
+        fabMainFacebook.setOnClickListener {
+            //todo implement facebook login
+        }
+    }
+
+    override fun updateImage(url: String) {
+        Glide.with(this)
+            .load(url)
+            .apply(
+                RequestOptions()
+                    .centerCrop()
+                    .error(R.drawable.ic_placeholder)
+            )
+            .into(civRecipeImage)
+    }
+
+    override fun showNetworkError() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Snackbar.make(clSnackBar, R.string.global_network_error, Snackbar.LENGTH_SHORT).apply {
+                view.elevation = 0f
+            }.show()
+        } else {
+            Toast.makeText(this, R.string.global_network_error, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun showAppTitle() {
+        tvMainTitle.visible()
+    }
+
+    override fun showProgressBar() {
+        pbMain.visible()
+    }
+
+    override fun hideProgressBar() {
+        pbMain.gone()
+    }
 
 }
--- app/src/main/java/com/kubiakpatryk/recipemaster/ui/main/MainView.kt
+++ app/src/main/java/com/kubiakpatryk/recipemaster/ui/main/MainView.kt
@@ -2,4 +2,16 @@ package com.kubiakpatryk.recipemaster.ui.main
 
 import com.kubiakpatryk.recipemaster.ui.base.BaseView
 
-interface MainView : BaseView
+interface MainView : BaseView {
+
+    fun showNetworkError()
+
+    fun hideProgressBar()
+
+    fun showAppTitle()
+
+    fun showProgressBar()
+
+    fun updateImage(url: String)
+
+}
